d18a7a18063062feac448f1d069fc9e8a831a730
m0hdShahrukh <mohdsharukh23@gmail.com>
2024-03-20 18:58:15 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+
 function App() {
   return (
-    <>
-      <Navbar logo="MOHD"/>
-      <div className="container">
-      <TextForm heading="Enter the text to analyze"/>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
